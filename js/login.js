50eef5766ebf929a99f94bc12c54a9fcfdb82986
Laureano73 <laureanotrilo@hotmail.com>
2023-08-17 19:40:34 -0300

    prueba

--- js/login.js
+++ js/login.js
@@ -1,48 +1,28 @@
-// Inicializando email.js
-emailjs.init('oIAWoFkg-W611XkrB')
+const btn = document.getElementById('button');
 
-// Obtener el formulario y botón de envío
-const form = document.querySelector('form')
-const enviarBtn = document.getElementById('enviarBtn')
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
 
-// Agregar un evento al formulario para el momento del envío
-form.addEventListener('submit', function (event) {
-    event.preventDefault()
+        btn.value = 'Enviando...';
 
-    // Cambia el valor del botón a "Enviando..."
-    enviarBtn.value = 'Enviando...'
+        const serviceID = 'default_service';
+        const templateID = 'template_ty0ex9r';
 
-    // ID del servicio y plantilla de email.js
-    const serviceID = 'service_axt837j'
-    const templateID = 'template_ty0ex9r'
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Enviar ';
+                alert('Sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
+    });
 
-    // Enviando el formulario a través de email.js
-    emailjs.sendForm(serviceID, templateID, this)
-        .then(() => {
-            // Cambia el valor del botón de nuevo
-            enviarBtn.value = 'Enviar mensaje'
-
-            // Muestra un mensaje de éxito
-            Swal.fire({
-                icon: 'success',
-                title: 'Mensaje Enviado 😀',
-                text: 'En breve un asesor se comunicará con vos.',
-                confirmButtonText: 'Cerrar'
-            })
-
-            // Restablece el formulario
-            form.reset()
-        })
-        .catch((error) => {
-            // Cambia el valor del botón de nuevo
-            enviarBtn.value = 'Enviar mensaje'
-
-            // Mostrar un mensaje de error con los detalles del error
-            Swal.fire({
-                icon: 'error',
-                title: 'Error al Enviar',
-                text: `Hubo un error al enviar el formulario: ${error}`,
-                confirmButtonText: 'Cerrar'
-            })
-        })
+Swal.fire({
+    icon: 'success',
+    title: 'Mensaje Enviado 😀',
+    text: 'En breve un asesor se comunicará con vos.',
+    confirmButtonText: 'Cerrar'
 })
+
